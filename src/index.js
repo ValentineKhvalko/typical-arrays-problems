fb5ef0226967d69cb02561462e7f2125353f24e2
Valentine <esfeeder@gmail.com>
2020-02-19 21:29:09 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(array === undefined) return 0;
+  if(array.length == 0) return 0;
+  return array.reduce(function (p, v) {
+    return ( p < v ? p : v );
+  });
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(array === undefined) return 0;
+  if(array.length == 0) return 0;
+  return array.reduce(function (p, v) {
+    return ( p > v ? p : v );
+  });
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(array === undefined) return 0;
+  if(array.length == 0) return 0;
+  let total = 0;
+  for(i = 0; i < array.length; i++){
+    total += array[i];
+  }
+  return total/array.length;
+};
